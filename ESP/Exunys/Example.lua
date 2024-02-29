ab79d43d6b9c817738e149c8d54f8697001974e5
ThatsMyMute <75107278+ThatsMyMute@users.noreply.github.com>
2024-02-28 20:51:44 -0500

    Create Example.lua

---
+++ ESP/Exunys/Example.lua
@@ -0,0 +1,6 @@
+local ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/Exunys/Exunys-ESP/main/src/ESP.lua"))()
+-- ESPLibrary and ExunysDeveloperESP is equivalent
+
+ESPLibrary.Load()
+
+-- ESPLibrary.UnwrapAll()
